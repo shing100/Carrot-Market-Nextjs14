64fdc10dc058d8990a6b94c815f75b044ebf9a9f
Lim Geun <shing100@naver.com>
2024-12-28 13:08:45 +0900

    feat: add npm i zod and createAccount actions

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,31 +1,37 @@
+"user client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import {createAccount} from "@/app/create-account/actions";
+import {useActionState} from "react";
+import Form from "next/form";
 
 export default function CreateAccount() {
+    const [state, dispatch] = useActionState(createAccount, null);
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Fill in the form below to join!</h2>
             </div>
-            <form className="flex flex-col gap-3">
-                <FormInput type="text" placeholder="Username" required errors={[]} />
-                <FormInput type="email" placeholder="Email" required errors={[]} />
+            <Form action={dispatch} className="flex flex-col gap-3">
+                <FormInput name="username" type="text" placeholder="Username" required errors={[]} />
+                <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
                 <FormInput
+                    name="password"
                     type="password"
                     placeholder="Password"
                     required
-                    errors={[]}
                 />
                 <FormInput
+                    name="passwordConfirm"
                     type="password"
                     placeholder="Confirm Password"
                     required
-                    errors={[]}
                 />
-                <FormButton loading={false} text="Create account" />
-            </form>
+                <FormButton text="Create account" />
+            </Form>
             <SocialLogin />
         </div>
     );
