191e1752139b46a214b840a5618b1bd8c9f17f13
Lim Geun <shing100@naver.com>
2024-12-28 12:09:08 +0900

    feat: add useActionState

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,28 +1,29 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 import Form from "next/form";
+import { useActionState } from "react";
+import {handleForm} from "@/app/login/actions";
 
 export default function LogIn() {
-    async function handleForm(formData: FormData) {
-        "use server";
-        console.log(formData.get("email"));
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-    };
+    const [state, action] = useActionState(handleForm, null);
+
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <Form action={handleForm} className="flex flex-col gap-3">
+            <Form action={action} className="flex flex-col gap-3">
                 <FormInput name="email" type="email" placeholder="Email" required errors={[]}/>
                 <FormInput
                     name="password"
                     type="password"
                     placeholder="Password"
                     required
-                    errors={[]}
+                    errors={state?.errors ?? []}
                 />
                 <FormButton text="Log in"/>
             </Form>
